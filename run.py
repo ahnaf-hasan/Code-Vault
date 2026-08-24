be34283a53dfdbbbc6aa3012c9262cdebd48e42b
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2026-08-24 17:41:02 +0600

    eight commit

---
+++ run.py
@@ -0,0 +1,48 @@
+x = "interesting Person"
+
+def myfunc():
+     print("He is " +  x)
+
+myfunc()
+
+
+a = "Good"
+
+def myfunc():
+ 
+  a = "Kind"
+
+  print("He is " + a)
+
+myfunc()
+
+print("He is " + a)
+
+
+
+def myfunc():
+
+    global i
+    i = "Wise"
+
+myfunc()
+
+print("He is " + i)
+
+
+
+x = "Interesting"
+
+print("Math is " + x)
+
+
+
+def myfunc():
+
+    global k 
+    k = "Subject"
+
+
+myfunc()
+
+print("Math is my favourite " + k)
