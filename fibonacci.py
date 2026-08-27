1e76f40fb726583a8a27345bb812d337593eb268
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2026-08-28 02:23:24 +0600

    tenth commit

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+def fibonacci_sequence(n):
+    sequence = [0, 1]
+    while len(sequence) < n:
+        sequence.append(sequence[-1] + sequence[-2])
+    return sequence
+
+
+num_terms = int(input("Enter the number of terms: "))
+
+
+if num_terms <= 0:
+    print("Please enter a positive integer")
+else:
+    print("Fibonacci sequence:")
+    for num in fibonacci_sequence(num_terms):
+        print(num)
