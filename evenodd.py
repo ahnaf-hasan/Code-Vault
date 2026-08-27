4d32ed7e14b985d7328e5d4b4c3d407e16d38bb4
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2026-08-28 02:31:03 +0600

    eleventh commit

---
+++ evenodd.py
@@ -0,0 +1,12 @@
+def check_odd_even(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+
+
+number = int(input("Enter a number: "))
+
+print(f"{number} is {check_odd_even(number)}.")
