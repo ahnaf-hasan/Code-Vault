506c478e82eed795bf8007f9a4eb2b8a2e7d03aa
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2026-09-01 18:08:29 +0600

    commit

---
+++ name.py
@@ -0,0 +1,12 @@
+def print_star_grid(rows, cols):
+    for row in range(rows):
+        for col in range(cols):
+            print('ahnaf tahmid hasan', end=' ')
+        print()
+
+
+rows = 7
+cols = 3
+
+
+print_star_grid(rows, cols)
