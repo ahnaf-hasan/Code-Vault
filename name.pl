e23d025b074b88b9d05860f138039f3d596af21e
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2026-09-10 04:14:29 +0600

    commit

---
+++ name.pl
@@ -0,0 +1,17 @@
+# Perl program 
+
+# A string scalar
+$name = "Ahnaf Hasan";
+
+# Integer scalar
+$rollno = 1;
+
+# A floating point scalar
+$percentage = 90.90;
+
+
+
+# To display the result
+print "Name = $name\n";
+print "Roll Number = $rollno\n";
+print "Percentage = $percentage\n";
