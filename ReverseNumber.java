1ed32ca3d0a6fd6695f194a927c0d71287d60def
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2026-09-17 21:43:55 +0600

    commit

---
+++ ReverseNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class ReverseNumber {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        int reversed = 0;
+
+        System.out.println("Original Number: " + num);
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println("Reversed Number: " + reversed);
+
+        input.close();
+    }
+}
+
